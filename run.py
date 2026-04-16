8ddd6dd4d7c6a52998c63659c62ef5739ee6ae74
daniel <azizbekrakhmonkulov01@gmail.com>
2026-04-17 01:25:06 +0900

    feat:python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
-a = 100
-print("a:", a)
+# message = "Hello World!"
+# print(message)
 
-message = "Hello World!"
-print(message)
+# first = "Men python o'rganishni boshladim!"
+# print(first)
 
-first = "Men python o'rganishni boshladim!"
-print(first)
+# norman = "MIT43 B group lider"
+# print(norman)
 
-norman = "MIT43 B group lider"
-print(norman)
+# print("Men GitHubni o'rnatim va test qilib korayapman")
 
-print("Men GitHubni o'rnatim va test qilib korayapman")
 
-practice = "Git va GitHubni takrorlash!"
-print(practice)
+# Dunder __builtins__, __init__
+message1 = "PYTHON: Everything is object!"
+print(message1)
+
+result = type(message1)
+print("result:", result)
+
+''' In Python, there are builtin tools:
+(1) TYPES > int, float, str, list, dict
+(2) FUNCTION > print() len() input() type()
+(3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))
