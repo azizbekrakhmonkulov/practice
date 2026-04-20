1c63677555c6857711640fd5dacb8d92476bf5ba
daniel <azizbekrakhmonkulov01@gmail.com>
2026-04-20 15:34:05 +0900

    feat:lear python objects

---
+++ objects.py
@@ -0,0 +1,27 @@
+'''OBJECTS
+(1) What is object
+(2) Iterable objects & RANGE
+(3) DICTIONARY
+(4) Error handling system
+'''
+
+import array  # package/module
+import math
+from math import ceil
+print("======What is object======")
+# An object has state and method propersties.
+# Everything is object in Python!
+
+print(type("Hello World"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigm > Functional Programming & OOP
+# OOP 4 Concepts > Abstraction |Encaptulation | Inheritence | Polimorphism
+result = math.ceil(97.7)  # CALL
+print("result:", result)
+
+result1 = ceil(98.7)
+print("result1:", result1)
