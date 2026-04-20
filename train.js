93139f105a9de0e267d1a95d4a147d2f7f03eb90
daniel <azizbekrakhmonkulov01@gmail.com>
2026-04-20 17:58:31 +0900

    fix: TASK-A and TASK-B

--- train.js
+++ train.js
@@ -1,20 +1,45 @@
-/* TASK-A
-❓ Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi
-parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-*/
+// TASK-B
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin,
+// hamda osha stringda
+// qatnashgan raqamlarni sonini bizga return qilsin.
 
 // ✅ Masalaning yechimi:
-function mevaHarfi(a, b) {
-    let soni = 0;
 
-    for (let harf of b) {
-        if (harf === a) {
-            soni++
+function countNumbers(str) {
+    let count = 0;
+
+    for (let char of str) {
+        if (char >= '0' && char <= '9') {
+            count++;
         }
     }
 
-    return soni;
+    return count;
 }
 
-let result = mevaHarfi("o", "avocado");
-console.log(result)
+let result = countNumbers("ajd212efdqfq");
+console.log("result:", result);
+
+
+// /* TASK-A
+// ❓ Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi
+// parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// */
+
+// // ✅ Masalaning yechimi:
+// function mevaHarfi(a, b) {
+//     let soni = 0;
+
+//     for (let harf of b) {
+//         if (harf === a) {
+//             soni++
+//         }
+//     }
+
+//     return soni;
+// }
+
+// let result = mevaHarfi("o", "avocado");
+// console.log(result)
+
