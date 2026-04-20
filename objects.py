29b7fd113142e9aeda4ef7c9f51d36531a199dfc
daniel <azizbekrakhmonkulov01@gmail.com>
2026-04-20 16:26:45 +0900

    feat: specific error handling system

--- objects.py
+++ objects.py
@@ -25,3 +25,20 @@ print("result:", result)
 
 result1 = ceil(98.7)
 print("result1:", result1)
+
+
+print("======Error handling system======")
+car_dict = dict(name="Tayota", year=2026, electric=True)
+try:
+    print("passed here")
+    a = car_dict.speed
+    result = car_dict["origin"]
+    print("result:", result)
+except KeyError as err:
+    print("No origin state property found:", err)
+except AttributeError as err:
+    print("No speed found:", err)
+else:
+    print("Executed successfylly without errors")
+finally:
+    print("Final closing logic")
