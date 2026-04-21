85e639bedec3589dc05bacefd7befc88e9061909
daniel <azizbekrakhmonkulov01@gmail.com>
2026-04-21 18:11:57 +0900

    feat:TASK-A-B-C

--- train.js
+++ train.js
@@ -1,25 +1,45 @@
+//TASK-C
+
+// ❓ Shunday function tuzing, u 2ta string parametr ega bolsin,
+//hamda agar har ikkala string bir hil harflardan iborat bolsa true
+//aks holda false qaytarsin
+
+// ✅ Masalaning yechimi:
+function sameLetters(a, b) {
+    let sort1 = a.split("").sort().join("");
+    let sort2 = b.split("").sort().join("");
+
+
+    return sort1 === sort2;
+
+
+}
+result = sameLetters("night", "thing");
+console.log(result);
+
+
 // TASK-B
 
-// Shunday function tuzing, u 1ta string parametrga ega bolsin,
+// ❓ Shunday function tuzing, u 1ta string parametrga ega bolsin,
 // hamda osha stringda
 // qatnashgan raqamlarni sonini bizga return qilsin.
 
 // ✅ Masalaning yechimi:
 
-function countNumbers(str) {
-    let count = 0;
+// function countNumbers(str) {
+//     let count = 0;
 
-    for (let char of str) {
-        if (char >= '0' && char <= '9') {
-            count++;
-        }
-    }
+//     for (let char of str) {
+//         if (char >= '0' && char <= '9') {
+//             count++;
+//         }
+//     }
 
-    return count;
-}
+//     return count;
+// }
 
-let result = countNumbers("ajd212efdqfq");
-console.log("result:", result);
+// let result = countNumbers("ajd212efdqfq");
+// console.log("result:", result);
 
 
 // /* TASK-A
